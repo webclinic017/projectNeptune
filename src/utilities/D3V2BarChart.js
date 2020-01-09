b0b1da5b2a79d93f07d00baba9f9b0c7cc9e8bca
Michael Ballard <bookstore707@gmail.com>
2020-01-09 13:46:37 -0500

    added multiple data intervals

--- src/utilities/D3V2BarChart.js
+++ src/utilities/D3V2BarChart.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
 
 // define margins for axises to display
-const MARGIN = {TOP: 10, BOTTOM: 50, LEFT:70, RIGHT:10}
+const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 70, RIGHT: 10 }
 
 // define width and height of the plot
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
@@ -12,69 +12,128 @@ const HEIGHT = 500 - MARGIN.BOTTOM - MARGIN.TOP;
 export default class BarChartV2 {
 
   constructor(element) {
+
+    // set this to this visualization
+    const vis = this;
+
     // construct svg, set l and w
-    const svg = d3.select(element)
+    vis.svg = d3.select(element)
       .append("svg")
-        .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-        .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+      .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+      .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
       .append("g")
-        .attr("transform",`translate(${MARGIN.LEFT},${MARGIN.TOP})`)
+      .attr("transform", `translate(${MARGIN.LEFT},${MARGIN.TOP})`)
 
-    d3.json(url)
-      .then(data => {
-        // set the max of my data
-        const max = d3.max(data, d => d.height)
+    // x axis append label
+    vis.svg.append("text")
+      .attr("x", WIDTH / 2)
+      .attr("y", HEIGHT + 50)
+      .attr("text-anchor", "middle")
+      .text("The worlds tallest men can't see me");
 
-        // create the y scale
-        const y = d3.scaleLinear()
-          .domain([
-            d3.min(data, d => d.height) * .95,
-             max
-            ])
-          .range([HEIGHT, 0])
+    // y axis append label
+    vis.svg.append("text")
+      .attr("x", -HEIGHT / 2)
+      .attr("y", -40)
+      .attr("text-anchor", "middle")
+      .text("Height in cm")
+      .attr("transform", `rotate(-90)`)
+      .attr("fill", "maroon");
 
-        const x = d3.scaleBand()
-          .domain(data.map(d => d.name))
-          .range([0, 800])
-          .padding(0.4)
 
-        // call to create the x axis
-        const xAxisCall = d3.axisBottom(x)
-        svg.append("g")
-          .attr("transform", `translate(0, ${HEIGHT})`)
-          .call(xAxisCall)
+    vis.xAxisGroup = vis.svg.append("g")
+      .attr("transform", `translate(0, ${HEIGHT})`)
 
-        // call to create the y axis
-        const yAxisCall = d3.axisLeft(y)
-        svg.append("g")
-          .call(yAxisCall)
 
-        // x axis append label
-        svg.append("text")
-            .attr("x", WIDTH / 2)
-            .attr("y", HEIGHT + 50)
-            .attr("text-anchor","middle")
-            .text("The worlds tallest men can't see me")
+    vis.yAxisGroup = vis.svg.append("g")
 
-        // y axis append label
-        svg.append("text")
-            .attr("x", -HEIGHT / 2)
-            .attr("y", -40)
-            .attr("text-anchor","middle")
-            .text("Height in cm")
-            .attr("transform", `rotate(-90)`)
-            .attr("fill", "maroon")
+    // create a promise to return all data sets
+    Promise.all([
+      d3.json("https://udemy-react-d3.firebaseio.com/tallest_men.json"),
+      d3.json("https://udemy-react-d3.firebaseio.com/tallest_women.json")
+    ]).then((datasets)=> {
+      
+      const [men, women] = datasets;
 
-        const rects = svg.selectAll("rect")
-          .data(data)
+      let flag = true;
 
-        rects.enter().append("rect")
-          .attr("x", d => x(d.name))
-          .attr("y", d => y(d.height))
-          .attr("width", x.bandwidth)
-          .attr("height", d => HEIGHT - y(d.height))
-          .attr("fill", "grey")
-      })
+      //set intervals for automatic updates
+        d3.interval(() => {
+          vis.data = flag ? men : women
+          vis.update()
+          flag =! flag
+        }, 1000)
+
+    
+    })
+
+    // create the data loading function
+    // d3.json(url)
+    //   .then(data => {
+
+    //     // set the data to my vis object
+    //     vis.data = data
+    //     // set intervals for automatic updates
+    //     d3.interval(() => {
+    //       vis.update()
+    //     }, 500)
+
+    //   })
   }
+
+  // create a function that updates each the graph changes
+  update() {
+
+    const vis = this;
+
+    // create the y scale
+    const y = d3.scaleLinear()
+      .domain([
+        d3.min(vis.data, d => d.height) * .95,
+        d3.max(vis.data, d => d.height)
+      ])
+      .range([HEIGHT, 0])
+
+    // create the x scale
+    const x = d3.scaleBand()
+      .domain(vis.data.map(d => d.name))
+      .range([0, 800])
+      .padding(0.4);
+
+    // call to calculate the x axis 
+    const xAxisCall = d3.axisBottom(x)
+    vis.xAxisGroup.call(xAxisCall);
+
+    // call to calculate the y axis
+    const yAxisCall = d3.axisLeft(y)
+    vis.yAxisGroup.call(yAxisCall);
+
+    // implement the data join
+    const rects = vis.svg.selectAll("rect")
+      .data(vis.data);
+
+    // create the exit selector
+    rects.exit().remove();
+
+    // create the update selector
+    rects
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", d => HEIGHT - y(d.height))
+
+    // create the enter selector
+    rects.enter().append("rect")
+      .attr("x", d => x(d.name))
+      .attr("y", d => y(d.height))
+      .attr("width", x.bandwidth)
+      .attr("height", d => HEIGHT - y(d.height))
+      .attr("fill", "grey");
+
+
+    console.log(rects)
+
+  }
+
 }
 
